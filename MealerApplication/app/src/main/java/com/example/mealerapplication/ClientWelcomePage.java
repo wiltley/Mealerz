44e0f83e8a4e5cb421775287b7193b735ea54190
DSL04 <souit02@outlook.com>
2022-10-15 18:20:44 -0400

    - Recreated the project with java as main programming language. Renamed the files to properly match their functionalities...ie ClientWelcomePage ect..

---
+++ MealerApplication/app/src/main/java/com/example/mealerapplication/ClientWelcomePage.java
@@ -0,0 +1,14 @@
+package com.example.mealerapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientWelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_welcome_page);
+    }
+}
---
+++ MealerApplication/app/src/main/java/com/example/mealerapplication/CookWelcomePage.java
@@ -0,0 +1,14 @@
+package com.example.mealerapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CookWelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook_welcome_page);
+    }
+}
