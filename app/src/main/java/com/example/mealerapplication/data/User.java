b2216bdd9dbc711d6dab47aac84689a39f5beca6
wiltley <65084149+wiltley@users.noreply.github.com>
2022-10-15 14:36:26 -0400

    Revert "Welcome"

---
+++ app/src/main/java/com/example/mealerapplication/data/User.java
@@ -0,0 +1,34 @@
+package com.example.mealerapplication.data;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+// CLIENT SIDE GENERAL USER
+
+public abstract class User {
+
+    private String username;
+    private String ID;
+
+    public User(){
+
+    }
+
+    public String getID(){
+
+        return ID;
+    }
+
+    public String getUsername(){
+
+        return username;
+
+    }
+
+    /*public void permissable(Permission p){
+
+    }*/
+
+
+}
