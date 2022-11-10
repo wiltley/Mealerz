855ba25b24d5fc32b0ea0f59194f49f13aa2d3ef
Wilt Moise <wiltmoise@gmail.com>
2022-11-10 13:52:07 -0500

    More refactoring + starting cook view

---
+++ app/src/main/java/com/example/mealerapplication/ui/cook/MyMealsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mealerapplication.ui.cook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyMealsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_meals);
+    }
+}
