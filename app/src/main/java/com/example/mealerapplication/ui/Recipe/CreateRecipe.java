b1b8ecdc72f15528012967c3edde49ef12fcd4d9
DSL04 <souit02@outlook.com>
2022-11-13 19:07:35 -0500

    - Added nav bar to certain cook activities. For now the buttons that can be routed are the meals view and the creation meals activity. Will later implement this nav functionality to Clients and Admin. For now there is a slight bug with the nav bar buttons, but the routing is still correct.

--- app/src/main/java/com/example/mealerapplication/ui/Recipe/CreateRecipe.java
+++ app/src/main/java/com/example/mealerapplication/ui/Recipe/CreateRecipe.java
@@ -2,6 +2,7 @@ package com.example.mealerapplication.ui.Recipe;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,30 +10,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.mealerapplication.R;
 import com.example.mealerapplication.data.accounthandling.CookHandler;
 import com.example.mealerapplication.data.model.Recipe;
+import com.example.mealerapplication.ui.cook.MyMealsActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class CreateRecipe extends AppCompatActivity {
 
     private final int GALLERY_REQ_CODE = 1000;
     ImageView imgGallery;
+    BottomNavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_recipe);
 
-        EditText recipeName = findViewById(R.id.new_meal_name);
-        EditText recipeCuisine = findViewById(R.id.new_meal_name);
-        EditText recipeDescription = findViewById(R.id.new_meal_description);
-        EditText recipePrice = findViewById(R.id.new_meal_price);
-        Button submitRecipe = findViewById(R.id.create_new_recipe);
+//        EditText recipeName = findViewById(R.id.new_meal_name);
+//        EditText recipeCuisine = findViewById(R.id.new_meal_name);
+//        EditText recipeDescription = findViewById(R.id.new_meal_description);
+//        EditText recipePrice = findViewById(R.id.new_meal_price);
+          Button submitRecipe = findViewById(R.id.create_new_recipe);
 
         Recipe recipe = new Recipe();
 
@@ -44,10 +51,10 @@ public class CreateRecipe extends AppCompatActivity {
 
 
 
-               recipe.setRecipeName(String.valueOf(recipeName.getText()));
-               recipe.setDescription(String.valueOf(recipeDescription.getText()));
-               recipe.setDescription(String.valueOf(recipeDescription.getText()));
-               recipe.setCuisineType(String.valueOf(recipeCuisine.getText()));
+//               recipe.setRecipeName(String.valueOf(recipeName.getText()));
+//               recipe.setDescription(String.valueOf(recipeDescription.getText()));
+//               recipe.setDescription(String.valueOf(recipeDescription.getText()));
+//               recipe.setCuisineType(String.valueOf(recipeCuisine.getText()));
 
                // We're definetely want to do some checks to see if the data
                // enter is valid before sending it to the database
@@ -84,5 +91,38 @@ public class CreateRecipe extends AppCompatActivity {
         //        imgGallery.setImageURI(data.getData());
         //}
     //}
+        nav = findViewById(R.id.btm_nav);
+
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.myMenu:
+                        Intent intent = new Intent(CreateRecipe.this, MyMealsActivity.class);
+                        startActivity(intent);
+
+
+                    case R.id.createFood:
+                        break;
+
+                    case R.id.requests:
+                        Toast.makeText(CreateRecipe.this, "requests", Toast.LENGTH_LONG).show();
+//                        Intent intent2 = new Intent(MyMealsActivity.this, .class);
+//                        startActivity(intent2);
+                        break;
+                    case R.id.myProfile:
+                        Toast.makeText(CreateRecipe.this, "profile", Toast.LENGTH_LONG).show();
+//                        Intent intent2 = new Intent(MyMealsActivity.this, .class);
+//                        startActivity(intent2);
+                        break;
+
+                    default:
+                }
+
+                return true;
+            }
+        });
     }
+
 }
