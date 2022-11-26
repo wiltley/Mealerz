c5d1cde661e8a20bc6f1d504f742ade8600af10c
DSL04 <souit02@outlook.com>
2022-11-26 17:09:33 -0500

    - added client nav bar, client search function

---
+++ app/src/main/java/com/example/mealerapplication/ClientSearchView.java
@@ -0,0 +1,59 @@
+package com.example.mealerapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientSearchView extends AppCompatActivity {
+    ListView listview;
+    SearchView search;
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_search_view);
+
+        spinner = findViewById(R.id.spinner);
+
+        List<String> search = new ArrayList<>();
+        search.add("Meal Type");
+        search.add("Cuisine Type");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, search);
+        spinner.setAdapter(adapter);
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.bottom_navigation_menu,menu);
+//
+//        MenuItem menuItem = menu.findItem(R.id.search);
+//        SearchView searchView = (SearchView) menuItem.getActionView();
+//        searchView.setQueryHint("Please type here");
+//
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String s) {
+//                return false;
+//            }
+//        });
+//
+//        return super.onCreateOptionsMenu(menu);
+//
+//    }
+}
